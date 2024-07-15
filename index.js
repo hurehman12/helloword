2218aac04260f832c28a40c10606307055458ae8
hurehman12 <haseeb.rehman@cccareers.org>
2024-07-15 11:38:25 -0400

    hello world server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code here
+//http
+const http = require('http');
+
+// Create the server object
+const server = http.createServer(function(req, res) {
+  // Log the request path
+  console.log('Received' + req.method + 'request for: ' + req.url);
+  
+  // Send headers
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  
+  // Send body response
+  res.end('Hello World');
+});
+
+
+// Add a listener for the server
+server.listen(8080, 'localhost', null, function() {
+  // Log a message when the server starts
+  console.log('Server is listening on localhost:8080');
+});
+
+
